2196522925c6565155682bd476b681416498fd5e
agraviog <agraviog@gmail.com>
2022-10-31 20:15:47 +0800

    verge animation

---
+++ script.js
@@ -0,0 +1,6 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
