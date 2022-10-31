3d5cc851c516ddb42ed9f6d38c52c2d92b9625c0
agraviog <agraviog@gmail.com>
2022-10-31 21:43:10 +0800

    gsap.min.js

--- verge.js
+++
@@ -1,6 +0,0 @@
-const { gsap } = require("gsap/dist/gsap");
-const { ScrollTrigger } = require("gsap/dist/ScrollTrigger");
-const { ScrollSmoother } = require("gsap/dist/ScrollSmoother");
-const { SplitText } = require("gsap/dist/SplitText");
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
