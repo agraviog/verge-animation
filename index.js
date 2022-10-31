668a7a054e0be3628348fc440de41fc2bf2ab390
agraviog <agraviog@gmail.com>
2022-10-31 21:04:46 +0800

    gsap.min.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import gsap from "/node_modules/gsap";
-import ScrollTrigger from "/node_modules/gsap/dist/ScrollTrigger";
-import SplitText from "/node_modules/gsap/dist/SplitText";
-import ScrollSmoother from "/node_modules/gsap/dist/ScrollSmoother";
-
-gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
-console.log("GSAP installed");
---
+++ script.js
@@ -0,0 +1,6 @@
+import ScrollTrigger from "/scripts/ScrollSmoother.min";
+import SplitText from "/scripts/SplitText.min";
+import ScrollSmoother from "/scripts/ScrollSmoother.min";
+
+gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
+console.log("GSAP installed");
