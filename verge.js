5498375aa2b15b95203b0bc6502771b9df63effd
agraviog <agraviog@gmail.com>
2022-11-02 22:09:50 +0800

    splitText

--- verge.js
+++ verge.js
@@ -4,3 +4,29 @@ let smoother = ScrollSmoother.create({
   smooth: 2,
   effects: true,
 });
+
+const childSplit = new SplitText("header h1", {
+  type: "lines, words",
+  linesClass: "split-child",
+});
+const parentSplit = new SplitText("header h1", {
+  linesClass: "split-parent",
+});
+
+// console.log(childSplit)
+const tl = gsap.timeline();
+tl.from(childSplit.lines, {
+  duration: 1.7,
+  yPercent: 100,
+  ease: "power4.out",
+  stagger: 0.1,
+}).to(
+  childSplit.lines,
+  {
+    duration: 1.7,
+    x: 39,
+    ease: "power4.out",
+    stagger: 0,
+  },
+  "+=1"
+);
