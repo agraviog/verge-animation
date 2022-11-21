e69a7696f5393042e8c5aca772ce0793a293d1ab
agraviog <agraviog@gmail.com>
2022-11-21 16:09:53 +0800

    smooth

--- host.js
+++ host.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 const smoother = ScrollSmoother.create({
-  smooth: 1.25,
+  smooth: 1,
   wrapper: "#smooth-wrapper",
   content: "#smooth-content",
   effects: true,
